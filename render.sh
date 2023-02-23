f8f467894a2d713c6e4ab00765205f18b0ab1da7
demogi4523 <demogi4523@yandex.ru>
2023-02-24 03:55:01 +0800

    (fix) Quick fix #22

--- render.sh
+++ render.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 poetry install
 
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 python manage.py migrate
 python manage.py createsuperuser --noinput

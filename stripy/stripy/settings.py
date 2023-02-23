339ef2b26761c93fab9bc48d5be834abbab51a48
demogi4523 <demogi4523@yandex.ru>
2023-02-24 04:00:04 +0800

    (fix) Quick fix #23

--- stripy/stripy/settings.py
+++ stripy/stripy/settings.py
@@ -186,3 +186,4 @@ if not DEBUG:
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    DEBUG = True
